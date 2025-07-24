90d60e1747934b8e14d5b4152499c77c46a339d8
Juee21 <khandalejuee@gmail.com>
2025-07-24 12:01:58 +0530

    chore: remove backend folder from frontend repo

--- backend/src/main/java/com/example/datavisualization/repository/DataPointRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datavisualization.repository;
-
-import com.example.datavisualization.model.DataPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataPointRepository extends JpaRepository<DataPoint, Long> {
-} 
